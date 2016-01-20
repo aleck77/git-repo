f694b34e1246e23f5a6ffcb31e19e9d4bf2c91b0
aleck77 <a.oshatjuk@gmail.com>
2016-01-21 01:47:59 +0200

    Создан класс выводящий время с 1.01.1970

---
+++ CurrentDateTime.java
@@ -0,0 +1,14 @@
+package leson1;
+
+public class CurrentDateTime {
+
+	public static void main(String[] args) {
+		long second=System.currentTimeMillis()/1000;
+		int minuts=(int)second/60;
+		int hour=minuts/60;
+		int day=hour/24;
+		System.out.println(day+":"+(hour-(day*24))+":"+(minuts-(hour*60))+":"+(second-(minuts*60)));
+		
+	}
+
+}
