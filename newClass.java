ad8c61a63ca7e9664e0673e36d70c67dc62927d1
aleck77 <a.oshatjuk@gmail.com>
2016-01-21 02:21:55 +0200

    Новый класс java

---
+++ newClass.java
@@ -0,0 +1,20 @@
+package leson1;
+
+public class Str {
+
+	public static void main(String[] args) {
+		String fName="Александр";
+		String lName="Ошатюк";
+		int age=39;
+		String space=" ";
+		String emptyLine="";
+		
+		System.out.print(fName);
+		System.out.print(space);
+		System.out.println(lName);
+		System.out.println(fName+space+lName+emptyLine+age);
+		
+
+	}
+
+}
